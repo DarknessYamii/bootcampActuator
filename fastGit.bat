d51dd2938dbb0c483e1bd572e797931aad051d4d
Andrés Ruiz Delgado <andresruizdelgado12@gmail.com>
2022-01-19 17:25:57 +0100

    first commit

---
+++ fastGit.bat
@@ -0,0 +1,7 @@
+echo "# bootcampActuator" >> README.md
+git init
+git add -A
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/DarknessYamii/bootcampActuator.git
+git push -u origin main
